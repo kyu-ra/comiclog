ae1dac7ec0bf41c147146e355fe7285183ac1426
Kyu <maili.ra.goodman@gmail.com>
2026-01-20 17:35:02 -0500

    added comic model

--- comiclogproject/log/models.py
+++ comiclogproject/log/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+# Information to log:
+    # Comic Name
+    # Issue number (if applicable)
+    # Publication year 
+    # Era (if applicable)
+    # Rating
+
+eras = [
+    ["Golden Age", datetime.date(1942,2,1)],
+    ["Silver Age", datetime.date(1956,10,1)],
+    ["Bronze Age", datetime.date(1970,4,1)],
+    ["Post-Crisis", datetime.date(1985,4,1)],
+    ["New 52", datetime.date(2011,8,31)],
+    ["Rebirth", datetime.date(2016,5,25)],
+    ["Infinite Frontier", datetime.date(2021,3,2)]
+]
+
+class Comic(models.Model):
+    comic_name = models.CharField("Comic Name", max_length=200)
+    issue_num = models.IntegerField("Issue Number", null=True)
+    pub_date = models.DateField("Date Published")
+    era = models.CharField("Era", max_length=200)
+    rating = models.IntegerField("Rating", null=True)
+
+    def __str__(self):
+        return self.comic_name
+    
+    def whatEra(self):
+        i = len(eras) - 1
+        while i < len(eras):
+            if(self.pub_date > eras[i][1]):
+                return eras[i]
+            else:
+                i -= 1
+ 
