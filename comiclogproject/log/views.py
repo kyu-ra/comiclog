c00ec4b7c67c3bec9aa3e9954a03e824b5ddaf87
Kyu <maili.ra.goodman@gmail.com>
2026-01-23 20:30:25 -0500

    update log index view

--- comiclogproject/log/views.py
+++ comiclogproject/log/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Comic
 
 def index(request):
-    return HttpResponse("hello world, you're at the log index")
+    latest_comic_list = Comic.objects.order_by("-pub_date")[:5]
+    context = {"latest_comic_list": latest_comic_list}
+    return render(request, "log/index.html", context)
 
 def detail(request, id):
     return HttpResponse("You're looking at comic %s." % id)
